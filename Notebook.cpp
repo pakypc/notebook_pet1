03444736e99f40b09373aa9fdb329f7934d5203d
den <dklim@mail.ru>
2025-04-16 22:21:47 +0700

    Создать функции меню

--- Notebook.cpp
+++ Notebook.cpp
@@ -50,6 +50,55 @@ void printList(Person *list, int n)
     cout << endl;
 }
 
+int menu ()
+{
+    cout << "\n1 - добавить запись\n";
+	cout << "2 - удалить запись\n";
+	cout << "3 - вывести без сортировки\n";
+	cout << "4 - вывести список, отсортированный по фамилии\n";
+	cout << "5 - поиск по фамилии\n";
+	cout << "6 - список именниников в определенном месяце\n";
+	cout << "7 - выход из программы\n";
+    int k;
+    cout << "Ваш выбор: ";
+    cin >> k;
+    while (k < 1 || k > 7) {
+        cout << "Неверный выбор! Повторите ввод: ";
+        cin >> k;
+    }
+    cout << endl;
+    return k;
+}
+
+void addContact(Person *list, int n)
+{
+    cout << "Добавление контакта\n";
+}
+
+void delContact(Person *list, int n)
+{
+    cout << "Удаление контакта\n";
+}
+
+void  printSort(Person *list, int n) 
+{
+	cout << "Сортировка по фамилии\n";
+}
+
+void findContact(Person *list, int n) 
+{
+	cout << "Поиск по фамилии\n";
+}
+
+void findBirthdays(Person *list, int n) 
+{
+	cout << "Поиск именинников\n";
+}
+
+void finish(Person *list, int n) {
+	cout << "Завершение работы\n";
+}
+
 int main ()
 {
     FILE* input = fopen("data.txt", "r");
@@ -76,8 +125,25 @@ int main ()
          cout << "Ошибка Закрытия файла для чтения\n";
     }
 
-    delete[] list;
-
-    return 0;
+     int vyb;
+     while (true) {
+         vyb = menu ();
+        switch (vyb) {
+		    case 1: addContact(list, n); 
+                    break;
+		    case 2: delContact(list, n); 
+                    break;
+	    	case 3: printList(list, n); 
+                    break;
+		    case 4: printSort(list, n); 
+                    break;
+		    case 5: findContact(list, n); 
+                    break;
+		    case 6: findBirthdays(list, n); 
+                    break;
+		    case 7: finish(list, n);
+                    delete[] list;
+                    return 0;
+		}
+     }
 }
-
