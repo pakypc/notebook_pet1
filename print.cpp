c79ebfa6250d7966f2f81fe5b69360a93654f4ce
den <dklim@mail.ru>
2025-04-17 17:15:01 +0700

    Сортировка контактов по фамилии

--- print.cpp
+++ print.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iomanip>
 #include "definitions.h"
+#include <cstring>
 
 using std::cout; 
 using std::endl;
@@ -21,4 +22,30 @@ void printList(Person *list, int n)
 void  printSort(Person *list, int n) 
 {
 	cout << "Сортировка по фамилии\n";
+    //создание и инициализация массива указателей
+    Person **ptr = new Person*[n];
+    for (int i = 0; i < n; ++i) {
+        ptr[i] = &list[i];
+    }
+    //сортировка массива указателей
+    for (int k = n - 1; k > 0; --k) {
+        for (int i = 0; i < k; ++i) {
+            if (strcmp(ptr[i]->secondName, ptr[i + 1]->secondName) > 0) {
+                Person *tmp = ptr[i];
+                ptr[i] = ptr[i + 1];
+                ptr[i + 1] = tmp;
+            }
+        }
+    }
+    //вывод на консоль
+    for (int i = 0; i < n; ++i) {
+        cout<<ptr[i]->secondName<<" "<<ptr[i]->name<<" тел.: "<<ptr[i]->telefon;
+        cout << "\n\t" << std::setw(2) << std::setfill('0') << ptr[i]->birthday.day
+            << "." << std::setw(2) << std::setfill('0')<< ptr[i]->birthday.month 
+            << "." << ptr[i]->birthday.year << "\n";
+    }
+    cout << endl;
+
+    //удаление массива указателей
+    delete[] ptr;
 }
